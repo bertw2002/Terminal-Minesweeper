8a81bd0dba9229c550fa2dd2d41e9efe297bda7f
Julian Wong <jwong01@stuy.edu>
2019-01-03 00:40:57 -0500

    add initial class variables

--- Tile.java
+++ Tile.java
@@ -1,3 +1,9 @@
 public class Tile {
+	private boolean Open; //Was this tile accessed?
+	private boolean Flagged; //Was this tile flagged?
+	private boolean Cleared; //Was this tile cleared?
+	private boolean Mine; //Is this tile a mine?
+	private int minesAround; //Number of mines around this tile in a 3x3 radius (corners included) (this is -1 if it's a mine)
+	
 	
 }
