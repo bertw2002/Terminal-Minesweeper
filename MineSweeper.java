5de063bd1f77ce6a6d39672189ba1bbb2b126260
bertw2002 <awan00@stuy.edu>
2019-01-04 11:02:39 -0500

    added constructor and main in minesweeper class and started tostring in board class

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,1 +1,26 @@
+public class MineSweeper{
+  public String toString(){
+    String board = "  ";
+    int counter = 1;
+    char alphabet = 'A';
+    for (int hor = 0; hor < hsize; hor ++){
+      board += counter;
+      counter ++;
+    }
+    for (int x = 0; x < hsize; x ++){
+      board += alphabet;
+      board += "|";
+      for (int y = 0; y < vsize; y ++){
+        board += Tile(x, y).toString();
+      }
+      alphabet ++;
+      board += "|";
+    }
+  }
+  public MineSweeper(int difficulty, int ){
 
+  }
+  public static void main(String[] args){
+
+  }
+}
