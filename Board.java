ee1d82fe802468ff0e27794d0810e973788ef7ea
Sublime <other@Alberts-MBP.fios-router.home>
2019-01-03 20:43:39 -0500

    Made the board class, added fields, and created the constructor and getTile() method.

--- Board.java
+++ Board.java
@@ -1,1 +1,19 @@
+public class Board{
+  private Tile[][] board; //makes the actual Board
+  private int hsize; //horizontal size
+  private int vsize; //vertical size
+  private int numMines; //number of mines
+  private int numFlags; //number of flags remaining
+  public Board(int minenum, int flagnum, int verticalsize, int horizontalsize){
+    hsize = horizontalsize;
+    vsize = verticalsize;
+    board = new Tile[hsize][vsize];
+    numMines = minenum;
+    numFlags = flagnum;
+  }
+  public Tile getTile(int xcor, int ycor){
+    return board[xcor][ycor];
+  }
 
+
+}
