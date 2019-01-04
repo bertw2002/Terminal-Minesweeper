20e3fc138c35e27bf284834557345f5ee6ab2345
Julian Wong <jwong01@stuy.edu>
2019-01-04 00:31:58 -0500

    wrote second Board() constructor, deleted numFlags initialization in constructor bc it's unnecessary

--- Board.java
+++ Board.java
@@ -4,12 +4,34 @@ public class Board{
   private int vsize; //vertical size
   private int numMines; //number of mines
   private int numFlags; //number of flags remaining
-  public Board(int minenum, int flagnum, int verticalsize, int horizontalsize){
+  public Board(int minenum, int verticalsize, int horizontalsize){ //constructor for custom board specs
     hsize = horizontalsize;
     vsize = verticalsize;
     board = new Tile[hsize][vsize];
     numMines = minenum;
-    numFlags = flagnum;
+  }
+  public Board(String difficulty) { //constructor that implements presets based on difficulty
+	if(difficulty == "easy" || difficulty == "Easy") {
+		hsize = 8;
+		vsize = 8;
+		board = new Tile[hsize][vsize];
+		numMines = 10;
+	}
+	else if(difficulty == "medium" || difficulty == "Medium") {
+		hsize = 16;
+		vsize = 16;
+		board = new Tile[hsize][vsize];
+		numMines = 40;
+	}
+	else if(difficulty == "hard" || difficulty == "Hard") {
+		hsize = 32;
+		vsize = 32;
+		board = new Tile[hsize][vsize];
+		numMines = 160;
+	}
+	else {
+		throw new IllegalArgumentException();
+	}
   }
   public Tile getTile(int xcor, int ycor){
     return board[xcor][ycor];
