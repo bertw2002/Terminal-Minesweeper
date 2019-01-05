e373a41f2cc20c20906b784a54ed0ee67be2c198
bertw2002 <awan00@stuy.edu>
2019-01-04 21:54:16 -0500

    commits not working.. test

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,8 +1,11 @@
 public class MineSweeper{
-  public MineSweeper(int difficulty){
-    
+  private Board board;
+  private double timer;
+  private String difficulty;
+  public MineSweeper(String diff){
+    difficulty = diff;
+    board = new board(difficulty);
   }
   public static void main(String[] args){
-
   }
 }
