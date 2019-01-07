339f4d2443b744c60c56c85335b83ff9b5d5179c
Julian Wong <jwong01@stuy.edu>
2019-01-06 23:21:25 -0500

    import scanner and currenttimemillis, edit constructor

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+import java.lang.*;
 public class MineSweeper{
   private Board board;
   private double timer;
   private String difficulty;
+  private boolean gameOver;
+  private double startTime;
   public MineSweeper(String diff){
     difficulty = diff;
     board = new Board(difficulty);
+	gameOver = false;
+	startTime = System.currentTimeMillis();
+	timer = System.currentTimeMillis() - startTime;
   }
   public static void main(String[] args){
   }
